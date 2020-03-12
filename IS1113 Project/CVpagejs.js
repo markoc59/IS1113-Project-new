95bdb2eaa6a65aac7c23aece225bed5549cfe4f8
markoc59 <62069150+markoc59@users.noreply.github.com>
2020-03-12 12:54:20 +0000

    Add files via upload

---
+++ IS1113 Project/CVpagejs.js
@@ -0,0 +1,22 @@
+function draw() {
+    var canvas = $('canvas')[0];
+    if(canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+    }
+}
+
+$(document).ready(function() {
+    // $(document).draw();
+    $('#content').animate({
+        opacity:1,
+        marginTop:'0',
+    }, 800);
+    $('h2').click(function() {
+        $(this).next('.subtext').slideToggle('fast');
+        $(this).children('.hex').toggleClass('moved');
+    })
+});
---
+++ IS1113 Project/MyInterests.js
@@ -0,0 +1,35 @@
+$('#scroller').imageScroll({
+
+ 
+
+  // top,right,bottom,left optional
+
+  orientation:"left",
+
+ 
+
+  // animation speed
+
+  speed:600,
+
+ 
+
+  // animation interval
+
+  interval:1500,
+
+ 
+
+  // pause on hover
+
+  hoverPause:true,
+
+ 
+
+  // callback function after every scroll motion
+
+  callback:function(){return false;}
+
+ 
+
+});
